c0f258213135c01dc1443e1b144e94144781649c
Yuri Lin <rye@mit.edu>
2010-10-27 15:49:02 -0400

    We want to compare classes by their FIRST block, not their LAST block

--- esp/esp/program/views.py
+++ esp/esp/program/views.py
@@ -39,7 +39,7 @@ from esp.users.models import ESPUser, UserBit, GetNodeOrNoBits, admin_required, 
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.db.models.query import Q
-from django.db.models import Max
+from django.db.models import Max, Min
 from django.db import transaction
 from django.core.mail import mail_admins
 from django.core.cache import cache
@@ -124,7 +124,7 @@ def lsr_submit(request, program = Program.objects.get(anchor__uri__contains="Spl
     already_flagged_secids = set(int(x.id) for x in already_flagged_sections)
     
     flag_related_sections = classes_flagged | classes_not_flagged
-    flagworthy_sections = ClassSection.objects.filter(id__in=flag_related_sections-already_flagged_secids).select_related('anchor').annotate(first_block=Max('meeting_times__start'))
+    flagworthy_sections = ClassSection.objects.filter(id__in=flag_related_sections-already_flagged_secids).select_related('anchor').annotate(first_block=Min('meeting_times__start'))
     
     sections_by_block = defaultdict(list)
     sections_by_id = {}
