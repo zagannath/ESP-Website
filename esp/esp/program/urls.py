cbbca65771202481a7685a4511feea57f111786d
Anthony Lu <lua@mit.edu>
2013-07-31 00:23:41 -0400

    stash@{2} on esp.mit.edu as of 2013/7/30

--- esp/esp/program/urls.py
+++ esp/esp/program/urls.py
@@ -13,3 +13,14 @@ urlpatterns = patterns('',
                        (r'^manage/preview/?$', 'esp.program.views.template_preview'),
                        (r'^manage/mergeaccounts/?$', 'esp.users.views.merge.merge_accounts'),
                        )
+
+# temporary redirects for Junction 2012
+from django.shortcuts import redirect
+urlpatterns += patterns('',
+                        (r'^learn/Junction/2012/archae/?$', lambda r: redirect('/mahara/view/view.php?t=HXo8cqOKyset4ALNhGbM')),
+                        (r'^learn/Junction/2012/biooc/?$', lambda r: redirect('/mahara/view/view.php?t=xfGjThwM42WAiyUqLQ5R')),
+                        (r'^learn/Junction/2012/neuro/?$', lambda r: redirect('/mahara/view/view.php?t=4X7wrRPZeC8buNpGVogY')),
+                        (r'^learn/Junction/2012/proteins/?$', lambda r: redirect('/mahara/view/view.php?t=QMfstlieXkJ0SjpDh9ma')),
+                        (r'^learn/Junction/2012/plato/?$', lambda r: redirect('/mahara/view/view.php?t=FYXAyDd3OUmcvTwt71Eb')),
+                        (r'^learn/Junction/2012/chelsea/?$', lambda r: redirect('/mahara/view/view.php?t=1ZoTfg5V2BLHnzYyhAux')),
+                        )
