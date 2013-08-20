9b896dd42763a868761825ed8526f5307c23b511
Jordan Moldow <jmoldow@mit.edu>
2013-08-20 13:31:35 -0700

    Modifies the proposed resources schema
    Makes some modifications based on the discussion at
    <https://github.com/learning-unlimited/ESP-Website/commit/4cdc12ab3c8508dc5f7b666114993274fb294463>.
    
    Does not yet make any of the major discussed modifications, such as
    merging Abstract, Type, and Family resource models into one model,
    pending more thought.

--- esp/esp/resources/models_new.py
+++ esp/esp/resources/models_new.py
@@ -1,60 +1,47 @@
-@reversion
-class Resource(models.Model):
+@reversion # Keep all previous versions of all objects, along with who made changes and when.
+class HistoryPreservingModel(models.Model) # All resource models will inherit from here, i.e. all models below have these properties.
+  is_active = models.BooleanField(default=True, help_text='Instead of deleting an object, set this to False. That way we retain access to old data and histories. Only objects with is_active=True will be used in views.')
+  class Meta:
+    abstract = True
+
+class Resource(HistoryPreservingModel):
   """
   An instance of a physical resource, e.g. Chromebook #1, ADP2 Mac adapter, etc.
   All resources exist globally for use by all programs.
   """
-  is_active = models.BooleanField()
   abstraction = models.ForeignKey(AbstractResource)
   identifier = models.CharField()
   # unique (abstraction, identifier) when is_active
-  availability = models.ManyToManyField(Event)
-  custom_attributes = models.JSONField()
-  admin_custom_attributes = models.JSONField()
-  description = models.TextField()
-  notes = models.TextField()
-  admin_notes = models.TextField()
+  availability = models.ManyToManyField(Event, help_text='All of the meeting times when this resource can be assigned to a class.') # Don't bother with a through model, because we can keep revisions here, and we don't need extra attributes for the through relation.
+  description = models.TextField(blank=True, default='', help_text='A description of the resource to be viewable by admins and teachers.')
 
-@reversion
-class AbstractResource(models.Model):
+class AbstractResource(HistoryPreservingModel):
   """
   Represents an abstract version of a specific type of Resource, e.g. Thinkpad Linux computer, mini-display-port to VGA Mac adapter, 6' chalkboard, etc.
   Resources of the same AbstractResource are identical.
   All abstract resources exist globally for use by all programs.
   """
-  is_active = models.BooleanField()
   resource_type = models.ForeignKey(ResourceType)
   name = models.CharField()
   # unique (resource_type, name) when is_active
-  is_reusable = models.BooleanField()
+  is_reusable = models.BooleanField(default=False, help_text="Can this resource be assigned more than once, or will its use in a class destroy it (such as a food item)? This defaults to True (can be reused), with the possibility of it being False (can't be reused, its use destroys it).")
   is_requestable = models.BooleanField()
-  custom_attributes = models.JSONField()
-  admin_custom_attributes = models.JSONField()
-  description = models.TextField()
-  notes = models.TextField()
-  admin_notes = models.TextField()
+  description = models.TextField(blank=True, default='', help_text='A description of the abstract resource to be viewable by admins and teachers.')
 
-@reversion
-class ResourceType(models.Model):
+class ResourceType(HistoryPreservingModel):
   """
   Represents a type of resource, e.g. Linux computer, Mac adapter, chalkboard, etc.
   Distinct AbstractResources in the same ResourceType are similar and can potentially serve as substitutes, but are not identical.
   All resource types exist globally for use by all programs.
   """
-  is_active = models.BooleanField()
   family = models.ForeignKey(ResourceFamily, null=True)
   name = models.CharField()
   # unique (resource_type, name) when is_active
   is_requestable = models.BooleanField()
   # if not family, then is_requestable
-  custom_attributes = models.JSONField()
-  admin_custom_attributes = models.JSONField()
-  description = models.TextField()
-  notes = models.TextField()
-  admin_notes = models.TextField()
+  description = models.TextField(blank=True, default='', help_text='A description of the resource type to be viewable by admins and teachers.')
 
-@reversion
-class ResourceFamily(models.Model):
+class ResourceFamily(HistoryPreservingModel):
   """
   Represents a family of resources, e.g. computers, A/V, boards, etc.
   Resources descended from the same ResourceFamily have something in common, but are not necessarily substitutes.
@@ -64,133 +51,123 @@ class ResourceFamily(models.Model):
   The trees will never be very tall, and probably not very wide, so this does not pose the problems that the DataTree has.
   All resource families exist globally for use by all programs.
   """
-  is_active = models.BooleanField()
   parent = models.ForeignKey(ResourceFamily, null=True)
   name = models.CharField()
   # unique (resource_type, name) when is_active
   is_requestable = models.BooleanField()
   # if not family, then is_requestable
-  custom_attributes = models.JSONField()
-  admin_custom_attributes = models.JSONField()
-  description = models.TextField()
-  notes = models.TextField()
-  admin_notes = models.TextField()
+  description = models.TextField(blank=True, default='', help_text='A description of the resource family to be viewable by admins and teachers.')
 
-@reversion
-class Location(models.Model):
+class Location(HistoryPreservingModel):
   """
   A.k.a. a classroom.
   All locations exist globally for use by all programs.
   """
-  is_active = models.BooleanField()
   area = models.ForeignKey(Area, null=True)
   name = models.CharField()
   # unique (area, name)
-  display_name_override = models.CharField() # Some string template that includes %(location)s and, optionally, %(area)s; or the empty string. If not empty, overrides area.display_name.
+  display_template_override = models.CharField(blank=True, default='') # Some string template that includes %(location)s and, optionally, %(area)s; or the empty string. If not empty, overrides area.display_template.
   capacity = models.IntegerField()
   furnishings = property() # AbstractResources, ResourceTypes, and ResourceFamilies
   availability = models.ManyToManyField(Event)
-  custom_attributes = models.JSONField()
-  teacher_custom_attributes = models.JSONField()
-  admin_custom_attributes = models.JSONField()
-  description = models.TextField()
-  notes = models.TextField()
-  teacher_notes = models.TextField()
-  admin_notes = models.TextField()
+  description = models.TextField(blank=True, default='', help_text='A description of the location to be viewable by admins, teachers, volunteers, and students.')
   is_requestable = models.BooleanField()
   url = models.URLField()
-  administrator_email = models.EmailField()
+  admins = models.ForeignKey(AreaAdministrativeGroup, null=True, help_text='The group that administrates this location and controls its usage, if different than that of the entire area.')
 
-@reversion
-class Area(models.Model):
+  def __unicode__(self):
+    """
+    display_template is self.display_template_override if it isn't blank;
+    and is otherwise self.area.display_template.
+
+    When printing the name of this Location, take display_template and
+    replace all occurances of %(area)s with self.area.__unicode__(),
+    and all occurances of %(location)s with self.name.
+    """
+    display_template = self.display_template_override or self.area.display_template
+    return display_template % {'area': self.area, 'location': self.name}
+
+class Area(HistoryPreservingModel):
   """
   A.k.a. building, or floor, or wing, etc.
   A set of Locations that are sufficiently close together for scheduling purposes, and form some distinct geographic unit.
   All areas exist globally for use by all programs.
   """
-  is_active = models.BooleanField()
   name = models.CharField()
-  display_name = models.CharField() # Some string template that includes %(location)s and, optionally, %(area)s. Applied to all Locations in the area for their __unicode__ method.
+  display_template = models.CharField(default='%(area)s %(location)s') # Some string template that includes %(location)s and, optionally, %(area)s. Applied to all Locations in the area for their __unicode__ method. See Location.__unicode__()
   adjacent_areas = models.ManyToMany(symmetric=True) # Other sets of Locations that are also sufficiently close together for scheduling purposes, e.g. adjacent buildings.
   latitude = models.DecimalField()
   longitude = models.DecimalField()
   map_pixel_x = models.IntegerField()
   map_pixel_y = models.IntegerField()
-  custom_attributes = models.JSONField()
-  teacher_custom_attributes = models.JSONField()
-  admin_custom_attributes = models.JSONField()
-  description = models.TextField()
-  notes = models.TextField()
-  teacher_notes = models.TextField()
-  admin_notes = models.TextField()
+  description = models.TextField(blank=True, default='', help_text='A description of the area to be viewable by anyone.')
   is_requestable = models.BooleanField()
   url = models.URLField()
-  administrator_email = models.EmailField()
+  admins = models.ForeignKey(AreaAdministrativeGroup, null=True, help_text='The group that administrates this area and controls its usage.')
 
-@reversion
-class Furnishing(models.Model):
+  def __unicode__(self):
+    return self.name
+
+class Furnishing(HistoryPreservingModel):
   """
   A permanent, fixed resource in a Location.
   Global for all programs.
   Can be an AbstractResource, a ResourceType, or a ResourceFamily, depending on the level of detail that is cared about.
   """
-  is_active = models.BooleanField()
-  custom_attributes = models.JSONField()
-  admin_custom_attributes = models.JSONField()
-  description = models.TextField()
-  notes = models.TextField()
-  admin_notes = models.TextField()
+  description = models.TextField(blank=True, default='', help_text='A description of the furnishing to be viewable by admins and teachers.')
   resource = models.ForeignKey(ContentType, choices=(AbstractResource,ResourceType,ResourceFamily))
   location = models.ForeignKey(Location)
   amount = models.IntegerField()
 
-@reversion
-class FloatingFurnishing(models.Model):
+class FloatingFurnishing(HistoryPreservingModel):
   """
   An assignment of a floating resource to a Location for some set of Events.
   """
-  is_active = models.BooleanField()
   resource = models.ForeignKey(Resource)
   location = models.ForeignKey(Location)
   meeting_times = models.ManyToManyField(Event)
   ignore_warnings = models.BooleanField()
   lock_level = models.IntegerField() # for autoscheduler
 
-@reversion
-class ResourceAssignment(models.Model):
+class ResourceAssignment(HistoryPreservingModel):
   """
   An assignment of a ClassSection to a Location and an Event, with Resources.
   """
-  is_active = models.BooleanField()
-  custom_attributes = models.JSONField()
-  admin_custom_attributes = models.JSONField()
-  notes = models.TextField()
-  admin_notes = models.TextField()
   resource = models.ManyToManyFields(Resource)
-  location = models.ForeignKey(Location)
+  location = models.ForeignKey(Location, help_text='The location of the class.')
   section = models.ForeignKey(ClassSection)
   meeting_time = models.ForeignKey(Event)
   # unique (resource, location, section) if is_active
   lock_level = models.IntegerField() # for autoscheduler
   ignore_warnings = models.BooleanField()
+  meeting_point = models.ForeignKey(Location, null=True, help_text='The meeting point for teachers and students to gather at the beginning of class, before walking to the actual class location.')
+  hide_location_from_students = models.BooleanField(default=False, help_true='Set this equal to True if the students should only be shown the meeting point, and not the location.')
+  instructions = models.TextField(default='', help_text='For teachers (and students, if there is a meeting point and hide_location_from_students is False) to see, instructions for getting from the meeting point to the actual location, and any other important information.')
 
-@reversion
-class ResourceRequest(models.Model):
+class ResourceRequest(HistoryPreservingModel):
   """
   A request for a ClassSubject (all of its ClassSections) to be assigned a resource.
   Can be either an AbstractResource, ResourceType, or ResourceFamily, depending on the allowed and needed level of detail
   (the model object requested must have is_requestable == True).
   """
-  is_active = models.BooleanField()
   resource = models.ForeignKey(ContentType, choices=(AbstractResource,ResourceType,ResourceFamily where is_requestable==True))
   subject = models.ForeignKey(ClassSubject)
   amount = models.IntegerField(null=True)
   pcnt_of_capacity = models.IntegerField(null=True)
-  description = models.TextField()
-  custom_attributes = models.JSONField()
-  admin_custom_attributes = models.JSONField()
-  notes = models.TextField()
-  admin_notes = models.TextField()
+  required = models.BooleanField(default=True, help_text='Do you absolutely need this resource for your class?')
+  description = models.TextField(blank=True, default='', help_text='A description of the resource request to be provided by teachers and viewable by admins.')
   wont_satisfy = models.BooleanField(default=False) # set True if the request has been denied
   is_satisfied_override = models.TextField(blank=True, default="") # If the request has been satisfied, but the requested Resource can't be assigned on the website, then set this field as a non-empty explanation string
-  
+
+class AreaAdministrativeGroup(HistoryPreservingModel):
+  name = models.CharField()
+  email = models.EmailField()
+  url = models.URLField()
+  description = models.TextField(help_text='A description of the group of administrators for a set of areas/locations, viewable by admins.')
+
+class AreaAdministrator(HistoryPreservingModel):
+  name = models.CharField()
+  email = models.EmailField()
+  url = models.URLField()
+  description = models.TextField(help_text='A description of an administrator for a set of areas/locations, viewable by admins.')
+  group = models.ForeignKey()
