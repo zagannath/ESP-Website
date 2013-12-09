e0c025632b4c9e533fd792f2bae7501e2f7156de
Gurtej Kanwar <gurtej@mit.edu>
2013-12-09 16:04:11 -0500

    Removes sibling discount tag from program creation
    The program creation form was giving a Whoops because that field on program doesn't exist any more.

--- esp/esp/program/views.py
+++ esp/esp/program/views.py
@@ -1,4 +1,3 @@
-
 __author__    = "Individual contributors (see AUTHORS file)"
 __date__      = "$DATE$"
 __rev__       = "$REV$"
@@ -484,7 +483,6 @@ def newprogram(request):
         line_items = pac.get_lineitemtypes(required_only=True).values('amount_dec')
 
         template_prog["base_cost"] = int(sum(x["amount_dec"] for x in line_items))
-        template_prog["sibling_discount"] = tprogram.sibling_discount_tag
 
     if 'checked' in request.GET:
         # Our form's anchor is wrong, because the form asks for the parent of the anchor that we really want.
